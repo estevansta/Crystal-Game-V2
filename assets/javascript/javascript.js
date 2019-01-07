73a9999d71b2ec49cb0139eebdf3ea89a8eb316d
Estevan Cruz <Estevancruz@Estevans-MacBook-Pro.local>
2019-01-06 19:45:38 -0600

    psuedo code for js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,8 @@
-console.log("heelo fam");
+// pseudocode notes
+// four crystals with a Random Result (19-120)
+// crystal values should be through 1-12
+// each crystal needs to be assigned a new random number that is generated every game as well as a random result(win/loss)
+// click fucntion on each crystal to display crystal number value that ADDS onto previous result (total score)
+// if total score = random result, WIN that incriments a win counter + win message; if not then lose and restart
+
+
